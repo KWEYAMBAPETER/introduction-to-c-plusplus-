e213b28d5f8df9599e562061adfe438f88adaa05
KWEYAMBAPETER <kweyamabapeter6@gmail.com>
2026-03-03 12:36:14 +0300

    add the scanf function

--- Sum.c
+++ Sum.c
@@ -8,7 +8,16 @@ int main()
     int c;
     c = a + b;
 
+    
+
     printf("the sum is %d", c); // the % sign is for allowing a string formatter/ placeholder(position of varible) and then put the value of the sum
+    
+    //attempt the user to input the valeus
+    int number1;
+    int number3;
+    int number2;
+     printf('Enter the first number `"with"`:>');
+     scanf("%d, &number1, &number2, &number3");
 
     return (0);
 }
