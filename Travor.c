e10c38c4285c3674b1f6a0d3db0c72435d0c7436
KWEYAMBAPETER <kweyamabapeter6@gmail.com>
2026-03-05 09:52:57 +0300

    Add the new code with variables lecture1

---
+++ Travor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main (){
+    printf("hello world!\n");
+    printf("The program has failed to run. I don't know the prob\n");
+    printf("The program has now run i have found the prob\n");
+
+
+    int a;
+    int b ;
+    int sum;
+    a=79;
+    b=39;
+    sum = a + b;
+    char grade = 'b';
+    float pi=3.14;
+
+    printf("the value of a is %d", a);
+    printf("\n the value of grade is %c", grade);
+    printf("\n the value of the float  is %f", pi);
+    printf("\nThe sum of a and b is %d", a+b);
+    
+    return 0;
+
+
+}
